0fd23c9bc7c29048812b9a57296531ec83ea3891
Tyowk <tyowk.personal@gmail.com>
2024-12-16 10:05:40 +0700

    Update and rename getUser.js to voteType.js

---
+++ src/functions/voteType.js
@@ -0,0 +1,13 @@
+module.exports = (d) => {
+    const data = d.util.aoiFunc(d);
+    const manager = d.client.topgg;
+    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Top.gg manager is not defined.`);
+    
+    const result = d.data.vote?.type;
+    if (!result) return d.client.returnCode(d, data);
+    
+    data.result = result;
+    return {
+        code: d.util.setCode(data)
+    }
+}
