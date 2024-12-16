c89abbc3449dc205829cb03d8a2b6e6811f512fa
Tyowk <tyowk.personal@gmail.com>
2024-12-16 22:41:53 +0700

    Update voteType.js

--- src/functions/voteType.js
+++ src/functions/voteType.js
@@ -1,9 +1,9 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
     const manager = d.client.topgg;
-    if (!manager) return d.aoiError.fnError(d, "custom", {}, `Top.gg manager is not defined.`);
+    if (!manager) return d.aoiError.fnError(d, 'custom', {}, `Top.gg manager is not defined.`);
     
-    const result = d.data.vote?.type;
+    const result = d.data.randoms?.vote?.type;
     if (!result) return d.client.returnCode(d, data);
     
     data.result = result;
