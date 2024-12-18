c7a6225f1023d112cfe3d7d3367a7e912f916529
Tyowk <tyowk.personal@gmail.com>
2024-12-18 19:55:21 +0700

    Update getBotInfo.js

--- src/functions/getBotInfo.js
+++ src/functions/getBotInfo.js
@@ -2,36 +2,21 @@ module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     const [id, property = 'username', sep = ', '] = data.inside.splits;
 
-    const botData = await d.client.topgg?.getBot(id);
-    const getBotData = {
-        invite: botData.invite,
-        website: botData.website,
-        support: botData.support,
-        github: botData.github,
-        longdesc: botData.longdesc,
-        shortdesc: botData.shortdesc,
-        prefix: botData.prefix,
-        lib: botData.lib,
-        clientid: botData.clientid,
-        avatar: botData.avatar,
-        id: botData.id,
-        username: botData.username,
-        date: botData.date,
-        server_count: botData.server_count,
-        shard_count: botData.shard_count,
-        guilds: botData.guilds,
-        shards: botData.shards,
-        monthlyPoints: botData.monthlyPoints,
-        points: botData.points,
-        certifiedBot: botData.certifiedBot,
-        owners: botData.owners?.join(sep),
-        tags: botData.tags?.join(sep),
-        bannerUrl: botData.bannerUrl,
-        donatebotguildid: botData.donatebotguildid
-    };
+    const manager = d.client.topgg;
+    if (!manager) return d.aoiError.fnError(d, 'custom', {}, `Top.gg manager is not defined.`);
+    
+    const botData = await manager?.getBot(id);
+    if (!botData) return d.aoiError.fnError(d, 'custom', {}, `Bot with id "${id}" not found`);
 
-    data.result = getBotData[property];
+    if (property === 'owners') {
+        data.result = botData.owners?.join(sep);
+    } else if (property === 'tags') {
+        data.result = botData.tags?.join(sep);
+    } else {
+        data.result = botData[property]
+    };
+    
     return {
         code: d.util.setCode(data)
-    }
+    };
 };
